4cace5dd1baacb5d1681c7e9aaaf67634d12c81a
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-04-28 00:59:00 -0400

    style: add header to notifications page

--- frontend/src/routes/NotificationHistory.jsx
+++ frontend/src/routes/NotificationHistory.jsx
@@ -4,22 +4,27 @@ export default function NotificationHistory() {
   const { kafkaData } = useOutletContext();
 
   return (
-    <div className="flex flex-col-reverse items-center gap-10 px-72">
-      {kafkaData.length ? (
-        kafkaData.map((item) => (
-          <div
-            className="flex h-16 w-2/3 items-center justify-center rounded-xl bg-secondary text-lg text-white"
-            key={item.datetime}
-          >
-            {`Todo ${item.action} at ${item.time} GMT on ${item.date}`}
-          </div>
-        ))
-      ) : (
-        <h1 className="pt-10 text-4xl font-bold">
-          There is no data or the websocket connection was closed. Refresh the page to
-          re-establish the connection.
-        </h1>
-      )}
-    </div>
+    <>
+      <h1 className="sticky top-0 mb-8 flex items-center justify-center bg-content/80 text-4xl font-bold">
+        Notification Center
+      </h1>
+      <div className="flex flex-col-reverse items-center gap-10 px-72">
+        {kafkaData.length ? (
+          kafkaData.map((item) => (
+            <div
+              className="flex h-16 w-2/3 items-center justify-center rounded-xl bg-secondary text-lg text-white"
+              key={item.datetime}
+            >
+              {`Todo ${item.action} at ${item.time} GMT on ${item.date}`}
+            </div>
+          ))
+        ) : (
+          <h2 className="mt-5 text-2xl font-semibold">
+            There is no data or the websocket connection was closed. Refresh the page to
+            re-establish the connection.
+          </h2>
+        )}
+      </div>
+    </>
   );
 }
