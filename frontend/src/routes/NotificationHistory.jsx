64b36f715d9b00219797f64cd5be443a2488737f
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-04-27 23:22:43 -0400

    style: add basic notification styling

--- frontend/src/routes/NotificationHistory.jsx
+++ frontend/src/routes/NotificationHistory.jsx
@@ -4,13 +4,20 @@ export default function NotificationHistory() {
   const { kafkaData } = useOutletContext();
 
   return (
-    <div>
+    <div className="flex flex-col items-center gap-10 px-72">
       {kafkaData.length ? (
-        kafkaData.map((item) => <div key={item.datetime}>{item.datetime}</div>)
+        kafkaData.map((item) => (
+          <div
+            className="flex h-16 w-2/3 items-center justify-center rounded-xl bg-secondary text-lg text-white"
+            key={item.datetime}
+          >
+            {`Todo ${item.action} at ${item.datetime}`}
+          </div>
+        ))
       ) : (
-        <h1>
+        <h1 className="pt-10 text-4xl font-bold">
           There is no data or the websocket connection was closed. Refresh the page to
-          reestablish the connection.
+          re-establish the connection.
         </h1>
       )}
     </div>
