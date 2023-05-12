3f48dfbe58e6583d9921c029150894e1be25da3f
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-05-12 14:08:21 -0400

    style: control width on individual elements

--- frontend/src/routes/NotificationHistory.jsx
+++ frontend/src/routes/NotificationHistory.jsx
@@ -7,24 +7,22 @@ export default function NotificationHistory() {
   return (
     <>
       <CustomHeader title="Notification Center" buttonType="reset"></CustomHeader>
-      <div className="xl:px-64">
-        <div className="flex flex-col-reverse items-center gap-10 ">
-          {kafkaData.length ? (
-            kafkaData.map((item) => (
-              <div
-                className="flex w-11/12 flex-grow items-center justify-center rounded-xl bg-gray-300 p-5 text-lg text-black shadow-xl md:w-2/3"
-                key={item.datetime}
-              >
-                {`Todo ${item.action} at ${item.time} GMT on ${item.date}`}
-              </div>
-            ))
-          ) : (
-            <h2 className="mt-5 text-2xl font-semibold">
-              There is no data or the websocket connection was closed. Refresh the page
-              to re-establish the connection.
-            </h2>
-          )}
-        </div>
+      <div className="flex flex-col-reverse items-center gap-10 ">
+        {kafkaData.length ? (
+          kafkaData.map((item) => (
+            <div
+              className="flex w-11/12 flex-grow items-center justify-center rounded-xl bg-gray-300 p-5 text-lg text-black shadow-xl md:w-2/3 xl:w-1/3"
+              key={item.datetime}
+            >
+              {`Todo ${item.action} at ${item.time} GMT on ${item.date}`}
+            </div>
+          ))
+        ) : (
+          <h2 className="mt-5 text-2xl font-semibold">
+            There is no data or the websocket connection was closed. Refresh the page to
+            re-establish the connection.
+          </h2>
+        )}
       </div>
     </>
   );
