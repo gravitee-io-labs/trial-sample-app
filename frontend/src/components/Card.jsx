ae1557ad8fff014c9574d3066f10ec9d37b92fe0
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-05-15 12:44:01 -0400

    feat: add cards to homepage

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+
+export default function Card({
+  title,
+  description,
+  architecture,
+  entrypoint,
+  endpoint,
+  planSecurity,
+  policies,
+}) {
+  return (
+    <Link
+      to="/todos"
+      className="h-96 w-72 border border-solid p-5 shadow-lg transition-transform duration-300 ease-in-out hover:translate-y-[-4px]"
+    >
+      <h3 className="">{title}</h3>
+      <div>{description}</div>
+      <br />
+      <div>
+        <em>Architecture: </em>
+        {architecture}
+      </div>
+      <div>
+        <em>Entrypoint: </em>
+        {entrypoint}
+      </div>
+      <div>
+        <em>Endpoint: </em>
+        {endpoint}
+      </div>
+      <div>
+        <em>Plan Security: </em>
+        {planSecurity}
+      </div>
+      <div>
+        <em>Policies: </em>
+        {policies}
+      </div>
+    </Link>
+  );
+}
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,6 +1,37 @@
+import Card from "../components/Card";
 import CustomHeader from "../components/CustomHeader";
 
-export default function NotificationHistory() {
+export default function Home() {
+  const tutorial_options = [
+    {
+      title: "Basic Security and Access Control",
+      description: "A basic example of a Gravitee's core features",
+      architecture: "http-proxy",
+      entrypoint: "REST",
+      endpoint: "REST",
+      planSecurity: "API Key",
+      policies: "Quota",
+    },
+    {
+      title: "Real-time Data and Protocol Mediation",
+      description: "A basic example of a Gravitee's core features",
+      architecture: "message introspection",
+      entrypoint: "Websocket",
+      endpoint: "Kafka",
+      planSecurity: "API Key",
+      policies: "Latency",
+    },
+    {
+      title: "Advanced Policies and Security",
+      description: "A basic example of a Gravitee's core features",
+      architecture: "message introspection",
+      entrypoint: "Websocket",
+      endpoint: "Kafka",
+      planSecurity: "JWT",
+      policies: "Assign Metrics",
+    },
+  ];
+
   return (
     <>
       <CustomHeader title="Home"></CustomHeader>
@@ -15,6 +46,11 @@ export default function NotificationHistory() {
           power of Gravitee API Management. Selecting a path will configure the
           application and open up the relevant documentation.
         </p>
+        <div className="mt-6 flex flex-col gap-5 lg:flex-row">
+          {tutorial_options.map((tutorial) => (
+            <Card {...tutorial} key={tutorial.title}></Card>
+          ))}
+        </div>
       </div>
     </>
   );
