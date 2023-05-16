9e5b2e5de8afce910932a610534a782d88ea7417
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-05-16 15:20:33 -0400

    refactor: move nodata page into its own component

---
+++ frontend/src/components/NoData.jsx
@@ -0,0 +1,10 @@
+export default function NoData() {
+  return (
+    <div className="flex items-center justify-center p-12">
+      <p className="text-xl font-semibold">
+        There is no data yet. Create, complete, archive, or delete todos to generate
+        data.
+      </p>
+    </div>
+  );
+}
--- frontend/src/routes/NotificationHistory.jsx
+++ frontend/src/routes/NotificationHistory.jsx
@@ -1,5 +1,6 @@
 import { useOutletContext } from "react-router-dom";
 import CustomHeader from "../components/CustomHeader";
+import NoData from "../components/NoData";
 
 export default function NotificationHistory() {
   const { kafkaData } = useOutletContext();
@@ -7,23 +8,20 @@ export default function NotificationHistory() {
   return (
     <>
       <CustomHeader title="Notification Center" buttonType="reset"></CustomHeader>
-      <div className="flex flex-col-reverse items-center gap-10 ">
-        {kafkaData.length ? (
-          kafkaData.map((item) => (
+      {kafkaData.length ? (
+        <div className="flex flex-col-reverse items-center gap-10 px-12">
+          {kafkaData.map((item) => (
             <div
               className="flex w-11/12 flex-grow items-center justify-center rounded-xl bg-gray-300 p-5 text-lg text-black shadow-xl md:w-2/3 xl:w-1/3"
               key={item.datetime}
             >
               {`Todo ${item.action} at ${item.time} GMT on ${item.date}`}
             </div>
-          ))
-        ) : (
-          <h2 className="font-semibold">
-            There is no data or the websocket connection was closed. Refresh the page to
-            re-establish the connection.
-          </h2>
-        )}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <NoData />
+      )}
     </>
   );
 }
