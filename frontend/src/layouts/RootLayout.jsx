d20f056b1e186687ccbb9fb44ee2062d6afe03bc
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-05-17 13:23:26 -0400

    chore: remove unnecessary svg attribute

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -95,7 +95,6 @@ export default function RootLayout() {
               xmlnsXlink="http://www.w3.org/1999/xlink"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              fit=""
               preserveAspectRatio="xMidYMid meet"
               focusable="false"
               className="w-10 fill-white"
