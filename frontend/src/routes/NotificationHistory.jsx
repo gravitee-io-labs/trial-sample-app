09e2ba15ceb60c40e4cdf8b1ea0a992ac07fc915
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-05-25 23:32:31 -0400

    style: update notification center

--- frontend/src/routes/NotificationHistory.jsx
+++ frontend/src/routes/NotificationHistory.jsx
@@ -12,7 +12,7 @@ export default function NotificationHistory() {
         <div className="flex flex-col-reverse items-center gap-10 px-12">
           {kafkaData.map((item) => (
             <div
-              className="flex w-11/12 flex-grow items-center justify-center rounded-xl bg-gray-300 p-5 text-lg text-black shadow-xl md:w-2/3 xl:w-1/3"
+              className="flex w-11/12 flex-grow items-center justify-center border-2 border-space-neutral-200 p-5 text-lg text-black shadow-lg md:w-2/3 xl:w-1/3"
               key={item.date + item.time}
             >
               {`Todo ${item.action} at ${item.time} ${item.timeZone} on ${item.date}`}
