8bb71339509bc5ac8b830e5c2944b42955e16b0d
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-08-07 18:41:27 -0400

    style: build base layout to merge analytics chart

--- frontend/src/routes/Todos.jsx
+++ frontend/src/routes/Todos.jsx
@@ -246,108 +246,120 @@ export default function Todos() {
   return (
     <>
       <CustomHeader title="Todo List"></CustomHeader>
-      <Tabs.Group
-        aria-label="Tabs with underline"
-        style="underline"
-        className="flex justify-center xl:px-72"
-        theme={{
-          tablist: {
-            tabitem: {
-              styles: {
-                underline: {
-                  active: {
-                    on: "text-accent-500 border-b-2 border-accent-500 !focus-visible:border-accent-500 !focus:border-accent-500",
+
+      <div className="flex h-screen flex-col gap-5 overflow-scroll px-1 xl:flex-row xl:px-10">
+        <div className="flex h-1/2 overflow-auto xl:h-full xl:w-1/2 xl:justify-center ">
+          <Tabs.Group
+            aria-label="Tabs with underline"
+            style="underline"
+            className="mx-3 w-full"
+            theme={{
+              tablist: {
+                styles: {
+                  underline: "justify-center",
+                },
+
+                tabitem: {
+                  styles: {
+                    underline: {
+                      active: {
+                        on: "text-accent-500 border-b-2 border-accent-500 !focus-visible:border-accent-500 !focus:border-accent-500",
+                      },
+                    },
                   },
                 },
               },
-            },
-          },
-        }}
-      >
-        <Tabs.Item title="Current" active={true}>
-          <div className="flex flex-col gap-5">
-            <input
-              type="text"
-              className="my-4 h-12 w-full rounded-lg bg-[#f3f3f3] p-2"
-              placeholder="+ Add a task. Press enter to save."
-              onChange={(e) => setNewTodo(e.target.value)}
-              value={newTodo}
-              onKeyDown={(e) => (e.key === "Enter" ? createTodo() : null)}
-            />
-            {sortedTodos
-              .filter((todo) => !todo.archive)
-              .map((todo) => (
-                <div
-                  className={`todo cursor-pointer gap-6 hover:bg-space-neutral-100/20 ${
-                    todo.complete ? "is-complete" : ""
-                  }`}
-                  key={todo._id}
-                  onClick={() => completeTodo(todo._id)}
-                >
-                  <div className="checkbox"></div>
-                  <div className="text">{todo.text}</div>
-                  <div
-                    className="ml-auto"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      archiveTodo(todo._id);
-                    }}
-                  >
-                    <FiArchive
-                      size="20"
-                      className=" text-black hover:text-accent-500"
-                    />
-                  </div>
-                  <div
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      deleteTodo(todo._id);
-                    }}
-                  >
-                    <FiTrash size="20" className="text-black hover:text-red-500" />
-                  </div>
-                </div>
-              ))}
-          </div>
-        </Tabs.Item>
-        <Tabs.Item title="Archived">
-          <div className="flex flex-col gap-5">
-            {todos
-              .filter((todo) => todo.archive)
-              .map((todo) => (
-                <div
-                  className={`todo gap-6 ${todo.complete ? "is-complete" : ""}`}
-                  key={todo._id}
-                >
-                  <div className="text">{todo.text}</div>
-                  <div
-                    className="ml-auto"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      archiveTodo(todo._id, false);
-                    }}
-                  >
-                    <FiInbox
-                      size="20"
-                      className="cursor-pointer text-black hover:text-accent-500"
-                    />
-                  </div>
-                  <div
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      deleteTodo(todo._id);
-                    }}
-                  >
-                    <FiTrash
-                      size="20"
-                      className="cursor-pointer text-black hover:text-red-500"
-                    />
-                  </div>
-                </div>
-              ))}
-          </div>
-        </Tabs.Item>
-      </Tabs.Group>
+              tabpanel: "p-0",
+            }}
+          >
+            <Tabs.Item title="Current" active={true}>
+              <div className="flex flex-col gap-5">
+                <input
+                  type="text"
+                  className="my-4 h-12 w-full rounded-lg bg-[#f3f3f3] p-2"
+                  placeholder="+ Add a task. Press enter to save."
+                  onChange={(e) => setNewTodo(e.target.value)}
+                  value={newTodo}
+                  onKeyDown={(e) => (e.key === "Enter" ? createTodo() : null)}
+                />
+                {sortedTodos
+                  .filter((todo) => !todo.archive)
+                  .map((todo) => (
+                    <div
+                      className={`todo cursor-pointer gap-6 hover:bg-space-neutral-100/20 ${
+                        todo.complete ? "is-complete" : ""
+                      }`}
+                      key={todo._id}
+                      onClick={() => completeTodo(todo._id)}
+                    >
+                      <div className="checkbox"></div>
+                      <div className="text">{todo.text}</div>
+                      <div
+                        className="ml-auto"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          archiveTodo(todo._id);
+                        }}
+                      >
+                        <FiArchive
+                          size="20"
+                          className=" text-black hover:text-accent-500"
+                        />
+                      </div>
+                      <div
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          deleteTodo(todo._id);
+                        }}
+                      >
+                        <FiTrash size="20" className="text-black hover:text-red-500" />
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </Tabs.Item>
+            <Tabs.Item title="Archived">
+              <div className="flex flex-col gap-5">
+                {todos
+                  .filter((todo) => todo.archive)
+                  .map((todo) => (
+                    <div
+                      className={`todo gap-6 ${todo.complete ? "is-complete" : ""}`}
+                      key={todo._id}
+                    >
+                      <div className="text">{todo.text}</div>
+                      <div
+                        className="ml-auto"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          archiveTodo(todo._id, false);
+                        }}
+                      >
+                        <FiInbox
+                          size="20"
+                          className="cursor-pointer text-black hover:text-accent-500"
+                        />
+                      </div>
+                      <div
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          deleteTodo(todo._id);
+                        }}
+                      >
+                        <FiTrash
+                          size="20"
+                          className="cursor-pointer text-black hover:text-red-500"
+                        />
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </Tabs.Item>
+          </Tabs.Group>
+        </div>
+        <hr />
+        <div className="h-1/2 w-1/2 xl:h-full">This is a test!</div>
+      </div>
     </>
   );
 }
