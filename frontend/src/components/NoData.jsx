d9edd57f41faa2bf556931ea5b3dcb20e46645b4
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-08-08 01:49:23 -0400

    style: move user analytics message to the top

--- frontend/src/components/NoData.jsx
+++ frontend/src/components/NoData.jsx
@@ -1,6 +1,6 @@
 export default function NoData({ message }) {
   return (
-    <div className="flex items-center justify-center py-12">
+    <div className="flex justify-center py-12">
       <p className="w-full text-center text-xl font-semibold">{message}</p>
     </div>
   );
