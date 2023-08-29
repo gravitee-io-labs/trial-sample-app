4f5794b662b274469c49dc71f3a8b2a428112e8e
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-08-28 20:35:52 -0400

    refactor: remove default userId for mongodb

--- backend/models/todo.js
+++ backend/models/todo.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
 const todoSchema = new Schema({
-  userId: { type: String, required: true, default: "root" },
+  userId: { type: String, required: true },
   text: { type: String, required: true },
   complete: { type: Boolean, default: false },
   archive: { type: Boolean, default: false },
