9e6e00a51216841d9ed33b6e1d041893d675819a
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-09-28 16:24:40 -0700

    feat: setup data pipelines

--- frontend/src/routes/StockMarket.jsx
+++ frontend/src/routes/StockMarket.jsx
@@ -1,5 +1,8 @@
-import { ResponsiveBar } from "@nivo/bar";
+import { ResponsiveLine } from "@nivo/line";
+import { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
+import useWebSocket from "react-use-websocket";
+import { v4 as uuidv4 } from "uuid";
 import CustomHeader from "../components/CustomHeader";
 
 const stockOptions = [
@@ -13,6 +16,11 @@ const stockOptions = [
   { name: "AWS", shares: "20", price: "$99" },
   { name: "Aklivity", shares: "20", price: "$99" },
 ];
+
+function convertUnixToLocale(unixTimestamp) {
+  const date = new Date(unixTimestamp);
+  return date.toLocaleString();
+}
 
 const countObjectsByProperty = (arr, prop) => {
   const counts = {};
@@ -34,8 +42,111 @@ const countObjectsByProperty = (arr, prop) => {
 };
 
 export default function StockMarket() {
-  const { kafkaData } = useOutletContext();
+  const { kafkaData, host } = useOutletContext();
   const graphData = countObjectsByProperty(kafkaData, "action");
+
+  // Ksqldb data streams share a consumer ID
+  const [ksqldbConsumerId] = useState(() => uuidv4());
+
+  // Cash balance data management
+  const [cashBalance, setCashBalance] = useState();
+  const { lastMessage: cashBalanceLastMessage } = useWebSocket(
+    `ws://${host}/stock-market/pksqlc-6v8k8CASH`,
+    {
+      queryParams: {
+        "X-Gravitee-Client-Identifier": ksqldbConsumerId,
+      },
+
+      shouldReconnect: () => true,
+      reconnectAttempts: 20,
+      onOpen: () => console.log("Cash Balance WebSocket opened"),
+      onError: (error) => console.log(`Cash Balance WebSocket error: ${error}`),
+      onClose: () => console.log("Cash Balance WebSocket closed"),
+    }
+  );
+  useEffect(() => {
+    if (cashBalanceLastMessage !== null) {
+      (async () => {
+        const data = await cashBalanceLastMessage.data.text();
+        const parsedData = JSON.parse(data);
+        setCashBalance(parsedData["CASH"]);
+      })();
+    }
+  }, [cashBalanceLastMessage]);
+
+  // Stock prices data management
+  const [stockPrices, setStockPrices] = useState([]);
+  const { lastMessage: stockPricesLastMessage } = useWebSocket(
+    `ws://${host}/stock-market/pksqlc-6v8k8CURRENT_STOCK_PRICES`,
+    {
+      queryParams: {
+        "X-Gravitee-Client-Identifier": ksqldbConsumerId,
+      },
+
+      shouldReconnect: () => true,
+      reconnectAttempts: 20,
+      onOpen: () => console.log("Stock prices WebSocket opened"),
+      onError: (error) => console.log(`Stock prices WebSocket error: ${error}`),
+      onClose: () => console.log("Stock prices WebSocket closed"),
+    }
+  );
+
+  useEffect(() => {
+    if (stockPricesLastMessage !== null) {
+      (async () => {
+        const data = await stockPricesLastMessage.data.text();
+        const parsedData = JSON.parse(data);
+        const {
+          key,
+          CURRENT_PRICE: currentPrice,
+          DATETIME: datetime,
+        } = parsedData;
+
+        setStockPrices((prevData) => {
+          key in prevData || (prevData[key] = []);
+          return {
+            ...prevData,
+            [key]: [...prevData[key], { currentPrice, datetime }],
+          };
+        });
+      })();
+    }
+  }, [stockPricesLastMessage]);
+
+  // Portfolio data management
+  const [portfolio, setPortfolio] = useState([]);
+  const { lastMessage: portfolioLastMessage } = useWebSocket(
+    `ws://${host}/stock-market/pksqlc-6v8k8PORTFOLIO`,
+    {
+      queryParams: {
+        "X-Gravitee-Client-Identifier": ksqldbConsumerId,
+      },
+
+      shouldReconnect: () => true,
+      reconnectAttempts: 20,
+      onOpen: () => console.log("Portfolio WebSocket opened"),
+      onError: (error) => console.log(`Portfolio WebSocket error: ${error}`),
+      onClose: () => console.log("Portfolio WebSocket closed"),
+    }
+  );
+
+  useEffect(() => {
+    if (portfolioLastMessage !== null) {
+      (async () => {
+        const data = await portfolioLastMessage.data.text();
+        const parsedData = JSON.parse(data);
+        const stock = parsedData["key"]["STOCK"];
+        delete parsedData.key;
+
+        setPortfolio((prevData) => {
+          return {
+            ...prevData,
+            [stock]: parsedData,
+          };
+        });
+      })();
+    }
+  }, [portfolioLastMessage]);
 
   return (
     <div className="flex h-screen flex-col">
@@ -44,55 +155,71 @@ export default function StockMarket() {
         <div className="flex h-full max-h-full flex-auto flex-col overflow-x-auto overflow-y-clip pb-10">
           <h2 className="m-0">Selected Stock</h2>
           <div className="h-[99%] flex-auto">
-            <ResponsiveBar
-              data={graphData}
-              keys={["quantity"]}
-              tooltip={({ id, value }) => (
-                <div
-                  style={{
-                    background: "white",
-                    color: "inherit",
-                    fontSize: "inherit",
-                    borderRadius: "2px",
-                    boxShadow: "0 1px 2px rgba(0, 0, 0, 0.25)",
-                    padding: "5px 9px",
-                  }}
-                >
-                  <strong>
-                    {id}: {value}
-                  </strong>
-                </div>
-              )}
-              indexBy="action"
-              margin={{ top: 30, right: 25, bottom: 80, left: 60 }}
-              padding={0}
-              valueScale={{ type: "linear" }}
-              indexScale={{ type: "band", round: true }}
-              colors={["#deebf7", "#c6dbef", "#9ecae1", "#6bafd6"]}
-              colorBy="index"
+            <ResponsiveLine
+              data={[
+                {
+                  id: "japan",
+                  color: "hsl(65, 70%, 50%)",
+                  data: stockPrices["gravitee"]
+                    ? stockPrices["gravitee"].map((item) => ({
+                        x: convertUnixToLocale(item.datetime),
+                        y: item.currentPrice,
+                      }))
+                    : [],
+                },
+              ]}
+              margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+              yFormat=" >-.2f"
+              axisTop={null}
+              axisRight={null}
               axisBottom={{
                 tickSize: 5,
                 tickPadding: 5,
-                tickRotation: 50,
-                legend: "Action",
+                tickRotation: 0,
+                legend: "transportation",
+                legendOffset: 36,
                 legendPosition: "middle",
-                legendOffset: 75,
               }}
               axisLeft={{
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: "Quantity",
+                legend: "count",
+                legendOffset: -40,
                 legendPosition: "middle",
-                legendOffset: -45,
               }}
-              labelSkipWidth={12}
-              labelSkipHeight={12}
-              labelTextColor={{
-                from: "color",
-                modifiers: [["darker", 1.6]],
-              }}
-              ariaLabel="Todo actions bar graph"
+              pointSize={10}
+              pointColor={{ theme: "background" }}
+              pointBorderWidth={2}
+              pointBorderColor={{ from: "serieColor" }}
+              pointLabelYOffset={-12}
+              useMesh={true}
+              legends={[
+                {
+                  anchor: "bottom-right",
+                  direction: "column",
+                  justify: false,
+                  translateX: 100,
+                  translateY: 0,
+                  itemsSpacing: 0,
+                  itemDirection: "left-to-right",
+                  itemWidth: 80,
+                  itemHeight: 20,
+                  itemOpacity: 0.75,
+                  symbolSize: 12,
+                  symbolShape: "circle",
+                  symbolBorderColor: "rgba(0, 0, 0, .5)",
+                  effects: [
+                    {
+                      on: "hover",
+                      style: {
+                        itemBackground: "rgba(0, 0, 0, .03)",
+                        itemOpacity: 1,
+                      },
+                    },
+                  ],
+                },
+              ]}
             />
           </div>
         </div>
