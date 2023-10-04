98359e56ab7c85231eb40ac94e6f54361ac1f917
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-10-04 14:23:38 -0400

    style: clean graph

--- frontend/src/routes/StockMarket.jsx
+++ frontend/src/routes/StockMarket.jsx
@@ -260,6 +260,25 @@ export default function StockMarket() {
                   })) ?? [{ x: 0, y: 0 }],
                 },
               ]}
+              tooltip={({ point }) => {
+                return (
+                  <div
+                    style={{
+                      background: "white",
+                      color: "inherit",
+                      fontSize: "inherit",
+                      borderRadius: "2px",
+                      boxShadow: "0 1px 2px rgba(0, 0, 0, 0.25)",
+                      padding: "5px 9px",
+                    }}
+                  >
+                    <div>
+                      Price: <strong>${point.data.y.toFixed(2)}</strong>
+                    </div>
+                  </div>
+                );
+              }}
+              margin={{ top: 50, right: 90, bottom: 50, left: 100 }}
               colors={() => "#009999"}
               yScale={{
                 type: "linear",
@@ -267,56 +286,32 @@ export default function StockMarket() {
                 min: "auto",
                 max: "auto",
               }}
-              margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-              yFormat=" >-.2f"
               axisBottom={{
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
                 legend: "Datetime",
-                legendOffset: 36,
+                legendOffset: 20,
                 legendPosition: "middle",
+                tickValues: [
+                  convertUnixToLocale(
+                    stockPrices[selectedStock]?.at(0)?.["datetime"]
+                  ),
+                  convertUnixToLocale(
+                    stockPrices[selectedStock]?.at(-1)?.["datetime"]
+                  ),
+                ],
               }}
               axisLeft={{
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
                 legend: "Price",
-                legendOffset: -40,
+                legendOffset: -50,
                 legendPosition: "middle",
               }}
-              pointSize={10}
-              pointColor={{ theme: "background" }}
-              pointBorderWidth={2}
-              pointBorderColor={{ from: "serieColor" }}
-              pointLabelYOffset={-12}
+              enablePoints={false}
               useMesh={true}
-              legends={[
-                {
-                  anchor: "bottom-right",
-                  direction: "column",
-                  justify: false,
-                  translateX: 100,
-                  translateY: 0,
-                  itemsSpacing: 0,
-                  itemDirection: "left-to-right",
-                  itemWidth: 80,
-                  itemHeight: 20,
-                  itemOpacity: 0.75,
-                  symbolSize: 12,
-                  symbolShape: "circle",
-                  symbolBorderColor: "rgba(0, 0, 0, .5)",
-                  effects: [
-                    {
-                      on: "hover",
-                      style: {
-                        itemBackground: "rgba(0, 0, 0, .03)",
-                        itemOpacity: 1,
-                      },
-                    },
-                  ],
-                },
-              ]}
             />
           </div>
           <div className="flex items-center gap-10 px-10 py-5">
