36d1da601259759c1073e5c1c75f11ecf691f4a8
Michael Field <81941044+bigmike36c@users.noreply.github.com>
2023-11-27 17:28:52 -0500

    docs: update homepage cards for quickstart update

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,7 +1,6 @@
-import { FiLock, FiZap } from "react-icons/fi";
+import { FiLock } from "react-icons/fi";
 import { useOutletContext } from "react-router-dom";
 import oneBar from "../assets/oneBar.svg";
-import twoBar from "../assets/twoBar.svg";
 import Card from "../components/Card";
 import CustomHeader from "../components/CustomHeader";
 
@@ -9,28 +8,28 @@ export default function Home() {
   const { fromPendoTutorial } = useOutletContext();
 
   const tutorial_options = [
+    // {
+    //   subtitle: "Beginner Use Case",
+    //   difficulty: "Beginner",
+    //   difficultyIcon: oneBar,
+    //   mainIcon: <FiZap size="40" />,
+    //   title: "Gateway APIs and Policies",
+    //   description:
+    //     "This tutorial demonstrates Gravitee's core features which include managing and deploying a Gateway API and applying API access restrictions with a policy.",
+    //   gatewayApiType: "Proxy",
+    //   entrypoint: "REST",
+    //   endpoint: "REST",
+    //   planSecurity: "Keyless",
+    //   policies: "Quota",
+    //   href: fromPendoTutorial
+    //     ? "https://documentation.gravitee.io/apim/getting-started/tutorials/comprehensive#trial-app-architecture"
+    //     : "https://documentation.gravitee.io/apim/getting-started/tutorials/comprehensive",
+    //   guideUser: true,
+    // },
     {
       subtitle: "Beginner Use Case",
       difficulty: "Beginner",
       difficultyIcon: oneBar,
-      mainIcon: <FiZap size="40" />,
-      title: "Gateway APIs and Policies",
-      description:
-        "This tutorial demonstrates Gravitee's core features which include managing and deploying a Gateway API and applying API access restrictions with a policy.",
-      gatewayApiType: "Proxy",
-      entrypoint: "REST",
-      endpoint: "REST",
-      planSecurity: "Keyless",
-      policies: "Quota",
-      href: fromPendoTutorial
-        ? "https://documentation.gravitee.io/apim/getting-started/tutorials/comprehensive#trial-app-architecture"
-        : "https://documentation.gravitee.io/apim/getting-started/tutorials/comprehensive",
-      guideUser: true,
-    },
-    {
-      subtitle: "Intermediate Use Case",
-      difficulty: "Intermediate",
-      difficultyIcon: twoBar,
       mainIcon: <FiLock size="40" />,
       title: "Security and Protocol Mediation",
       description:
@@ -62,23 +61,23 @@ export default function Home() {
   return (
     <>
       <CustomHeader title="Home"></CustomHeader>
-      <div className="flex w-full flex-col items-center px-12 gap-5">
-        <h2>{"Welcome to Gravitee's API Management Trial App"}</h2>
+      <div className="flex w-full flex-col items-center gap-5 px-12">
+        <h2>{"Welcome to Gravitee's API Management Sample App"}</h2>
         {fromPendoTutorial ? (
           <p>
-            To learn how to test your Gateway API, first open the relevant documentation
-            by selecting the highlighted <strong>Gateway APIs and Policies</strong> card
-            below!
+            To learn how to test your Gateway API, first open the relevant
+            documentation by selecting the highlighted
+            <strong>Gateway APIs and Policies</strong> card below!
           </p>
         ) : (
           <>
             <p>
-              This app has been built for you to experience the full benefits of API
-              Management with your trial.
+              This sample app has been built to showcase Gravitee API Management
+              (APIM) functionality with practical use cases.
             </p>
             <p>
               {
-                "The beginner use case: Gateway APIs and Policies, should be completed first. The other use cases can then be completed in any order to experience the breadth of Gravitee API Management's (APIM) capabilities. Selecting a path will open up the relevant documentation in a new tab."
+                "The available use cases can be completed in any order to experience the breadth of Gravitee APIM's capabilities. Selecting a path will open up the relevant documentation in a new tab."
               }
             </p>
           </>
